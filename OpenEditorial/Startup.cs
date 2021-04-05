fd372930dc6196a2e01b9cab3e38bbb4ed9f1dc1
openeditorialteam <81228935+openeditorialteam@users.noreply.github.com>
2021-04-05 11:17:44 +0300

    piranha cms setup

--- OpenEditorial/Startup.cs
+++ OpenEditorial/Startup.cs
@@ -4,10 +4,14 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Piranha;
+using Piranha.AttributeBuilder;
+using Piranha.Data.EF.SQLite;
 
 namespace OpenEditorial
 {
@@ -23,11 +27,30 @@ namespace OpenEditorial
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            //services.AddControllersWithViews();
+
+            services.AddPiranha(options =>
+            {
+                options.UseFileStorage(naming: Piranha.Local.FileStorageNaming.UniqueFolderNames);
+                options.UseImageSharp();
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseMemoryCache();
+                options.UseEF<SQLiteDb>(db =>
+                    db.UseSqlite("Filename=./PiranhaWeb.db"));
+            });
+
+            services.AddPiranhaSimpleSecurity(
+                new Piranha.AspNetCore.SimpleUser(Piranha.Manager.Permission.All())
+                {
+                    UserName = "admin",
+                    Password = "password"
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApi api)
         {
             if (env.IsDevelopment())
             {
@@ -42,15 +65,23 @@ namespace OpenEditorial
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            /*app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });*/
+
+            App.Init(api);
+
+            app.UsePiranha(options => {
+                options.UseManager();
+                options.UseTinyMCE();
+                //options.UseIdentity();
             });
         }
     }
